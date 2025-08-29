60038ab324bcd081f19bd511d8f34ac95dd2cda8
VishalKandagatla <k.vishal1234@gmail.com>
2025-08-29 06:49:48 +0530

    Add ESLint config and sample test

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-unused-vars': 'off',
+  },
+};
